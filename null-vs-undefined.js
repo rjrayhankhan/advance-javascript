bdc109b9049e22c41c62146f6dc1411845e96dd2
rjrayhankhan <aburayhan6212@gamil.com>
2020-12-11 15:37:05 +0600

    good evening

---
+++ null-vs-undefined.js
@@ -0,0 +1,17 @@
+//let pakhi;
+
+// function add(num1, num2){
+//     console.log(num1 + num2);
+//     return
+// }
+
+// function add (num1, num2){
+//     console.log(num1, num2);
+// }
+
+// const pramik = { name:"smart boy", phone:"10215361"}
+
+// console.log(pramik.gf);
+
+let fun = undefined;
+console.log(fun);
